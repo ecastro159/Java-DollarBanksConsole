ed4d1523d2ebdcbdb5b5ffc21f86a0edc7a189d7
Erwin Castro <90945345+ecastro159@users.noreply.github.com>
2023-02-06 11:58:48 -0500

    Setup project1

---
+++ dollarsbank/src/main/java/com/cognixia/jump/App.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
