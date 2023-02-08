ce2cda47507e3d5262c84870660a8127f196985c
Erwin Castro <90945345+ecastro159@users.noreply.github.com>
2023-02-08 09:48:04 -0500

    Table changes

--- CustomerTable.sql
+++ CustomerTable.sql
@@ -1,9 +1,11 @@
 create database Project1Java;
 use Project1Java;
+drop table customer;
 CREATE TABLE customer (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     contact_number VARCHAR(50) NOT NULL,
     user_password VARCHAR(50) NOT NULL
-)
+);
+ALTER TABLE customer AUTO_INCREMENT = 1000;
